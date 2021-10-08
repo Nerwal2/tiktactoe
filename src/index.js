e0725424b9ec732786189646afd13fb367bdd01c
Nerwal2 <master4567890@gmail.com>
2021-10-08 11:26:18 +0000

    update

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+
+class Square extends React.Component{
+    constructor(props){
+        super(props);
+    }
+render(){
+    return (
+        <button
+        onClick={() => {
+            this.props.onClick();
+        }}
+        >
+            {this.props.value}
+        </button>
+    );
+}
+}
+class Board extends React.Component{
+    constructor(props){
+    super(props);
+}
+
+render(){
+    return this.props.squares.map((arrayElement, index) => {
+        const arrowSquareFunction = () => this.props.handleSquareClick(index);
+        return (
+            <Square
+            key={index}
+            onClick={arrowSquareFunction}
+            value={arrayElement}
+            />
+        );
+    });
+    }
+}
+
+
+class Game extends React.Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        squares: Array(9).fill(null),
+        nextSymbol: "O"
+        };
+        this.handleSquareClick = this.handleSquareClick.bind(this);
+    }
+handleSquareClick(index){
+    const stateSquares = this.state.squares;
+    stateSquares[index] = this.state.nextSymbol;
+    const nextSymbol = this.state.nextSymbol === "X" ? "O" : "X";
+    this.setState({ squares: stateSquares, nextSymbol: nextSymbol});
+}
+render(){
+return (
+    <Board
+    squares={this.state.squares}
+    handleSquareClick={this.handleSquareClick}
+    />
+);
+}
+}
+
+
+ReactDOM.render(<Game />, document.getElementById("root"));
