e2f0539a660854a942a9bbc40c6ff44dab863dd1
Nerwal2 <master4567890@gmail.com>
2021-10-11 12:27:39 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM, { render } from "react-dom";
+import "./index.css";
 
 class Square extends React.Component{
     constructor(props){
@@ -12,7 +13,7 @@ render(){
             this.props.onClick();
         }}
         >
-            {this.props.value}
+            {this.props.value || <div>&nbsp;</div>}
         </button>
     );
 }
@@ -22,19 +23,15 @@ class Board extends React.Component{
     super(props);
 }
 
-render(){
-    return this.props.squares.map((arrayElement, index) => {
-        const arrowSquareFunction = () => this.props.handleSquareClick(index);
-        return (
-            <Square
-            key={index}
-            onClick={arrowSquareFunction}
-            value={arrayElement}
-            />
-        );
+render() {
+    return this.props.squares.map((arrayElements, index) => {
+      return <>
+       <Square key={index} onClick={()=>this.props.handleSquareClick(index)} value={arrayElements} />
+       {(index + 1) % 3 === 0 ? <br/> : <></>}
+     </>
     });
-    }
-}
+   }
+ }
 
 
 class Game extends React.Component {
@@ -46,9 +43,42 @@ class Game extends React.Component {
         };
         this.handleSquareClick = this.handleSquareClick.bind(this);
     }
+    checkWinning(squares){
+        const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],    
+        ];
+        for(let i = 0;i < winningCombinations.length;i++) {
+            let [a,b,c] = winningCombinations[i]
+            if(
+                (squares[a] ==="X" || squares[a] === "O") &&
+                squares[a] === squares [b] &&
+                squares[a] === squares [c]
+            ) {
+                return winningCombinations[i];
+            }
+            
+        }
+    }
 handleSquareClick(index){
     const stateSquares = this.state.squares;
+    if (stateSquares[index] !== null || this.state.gameWon) {
+        return;
+      }
     stateSquares[index] = this.state.nextSymbol;
+    let winningCombination = this.checkWinning(stateSquares);
+    if (winningCombination) {
+     
+      alert(`Win ${winningCombination}`);
+
+      this.setState({ gameWon: true });
+    }
     const nextSymbol = this.state.nextSymbol === "X" ? "O" : "X";
     this.setState({ squares: stateSquares, nextSymbol: nextSymbol});
 }
